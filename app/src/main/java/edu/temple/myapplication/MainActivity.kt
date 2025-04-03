bedf33fe3bfdd7b53001555107b6ffc54671c138
Maab Taha <tmaab13@gmail.com>
2025-04-03 12:07:16 -0400

    countdown in logcat

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,50 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+
+    var timerBinder: TimerService.TimerBinder? = null
+    var isConnected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        val serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+                timerBinder = p1 as TimerService.TimerBinder
+            }
 
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                timerBinder = null
+            }
+
+        }
+
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
+
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            if(timerBinder?.isRunning == true) {
+                timerBinder!!.pause()
+            } else {
+                timerBinder?.start(25)
+            }
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            timerBinder?.stop()
         }
     }
 }
